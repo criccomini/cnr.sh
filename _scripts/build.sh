07b29f4b8575f1d920c09c03952bd318736c3008
Chris Riccomini <criccomini@apache.org>
2021-02-18 15:20:34 -0800

    Add OG description

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -7,16 +7,17 @@ echo "essay:" > $ESSAYS_YAML
 for ESSAY_MD in $(ls -r _essays); do
   # Trim XXXX-XX-XX date and .md from filename
   ESSAY=$( echo "$ESSAY_MD" | cut -c 12- | rev | cut -c 4- | rev )
+  ESSAY_HTML="$ESSAY.html"
   TITLE=$( sed -n '/title:/p' _essays/$ESSAY_MD  | head -n 1 | cut -c 8- )
   DATE=$( sed -n '/date:/p' _essays/$ESSAY_MD | head -n 1 | cut -c 7- )
+  # That gnarly sed is to remove markdown links. Yeah.
+  DESCRIPTION=$( sed -n '6 p' _essays/$ESSAY_MD | sed "s|\[\([^]]*\)\](\([^)]*\))|\1|g" | tr -d '"' | cut -c-200)
 
   echo "  - title: $TITLE" >> $ESSAYS_YAML
   echo "    date: $DATE" >> $ESSAYS_YAML
   echo "    dir: $ESSAY" >> $ESSAYS_YAML
 
-  ESSAY_HTML="$ESSAY.html"
-  V_ESSAY="$V_ESSAY -V 'essay.title=$TITLE' -V 'essay.date=$DATE' -V 'essay.dir=$ESSAY'"
-  pandoc --standalone -f markdown-implicit_figures --template _templates/essay.html _essays/$ESSAY_MD -o essays/$ESSAY_HTML
+  pandoc --standalone -f markdown-implicit_figures --template _templates/essay.html -Mdescription="$DESCRIPTION" _essays/$ESSAY_MD -o essays/$ESSAY_HTML
 done
 
 # Render essays index page
