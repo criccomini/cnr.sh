4e88a8f52f7d467b4e46816d2533d367b455b666
Chris Riccomini <criccomini@apache.org>
2021-02-20 15:33:48 -0800

    Update font awesome

--- _templates/tracker.js
+++ _templates/tracker.js
@@ -1,7 +1,7 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-3R91BN0RW0"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-3R91BN0RW0');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3R91BN0RW0');
 </script>
