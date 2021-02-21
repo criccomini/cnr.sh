7f5abf51b93572d7d60bcb0d8aa1c5d963dad92d
Chris Riccomini <criccomini@apache.org>
2021-02-21 14:01:54 -0800

    Add robots.txt and sitemap.txt

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,9 +2,12 @@
 
 #pandoc --standalone -f markdown-implicit_figures --template _templates/essay.html _markdown/2019-07-29-future-data-engineering.md -o essays/future-data-engineering.html
 ESSAYS_YAML=_build/essays.yaml
+SITEMAP_TXT=sitemap.txt
 echo "essay:" > $ESSAYS_YAML
+rm $SITEMAP_TXT
 
-for ESSAY_MD in $(ls -r _essays); do
+# Render essays
+for ESSAY_MD in $( ls -r _essays ); do
   # Trim XXXX-XX-XX date and .md from filename
   ESSAY=$( echo "$ESSAY_MD" | cut -c 12- | rev | cut -c 4- | rev )
   ESSAY_HTML="$ESSAY.html"
@@ -16,6 +19,8 @@ for ESSAY_MD in $(ls -r _essays); do
   echo "  - title: $TITLE" >> $ESSAYS_YAML
   echo "    date: $DATE" >> $ESSAYS_YAML
   echo "    dir: $ESSAY" >> $ESSAYS_YAML
+
+  echo "http://cnr.sh/essays/$ESSAY" >> $SITEMAP_TXT
 
   pandoc --standalone -f markdown-implicit_figures --template _templates/essay.html -Mdescription="$DESCRIPTION" _essays/$ESSAY_MD -o essays/$ESSAY_HTML
 done
