b4c18154e5f79a4edd420917d1c82ce3ec7b2a8f
Chris Riccomini <criccomini@apache.org>
2021-02-24 08:34:41 -0800

    Convert sitemap to xml

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-ESSAYS_YAML=_yaml/essays.yml
-TALKS_YAML=_yaml/talks.yml
-SITEMAP_TXT=sitemap.txt
-echo "links:" > $ESSAYS_YAML
-echo "https://cnr.sh/essays" > $SITEMAP_TXT
-echo "https://cnr.sh/talks" >> $SITEMAP_TXT
+ESSAYS_YML=_yaml/essays.yml
+TALKS_YML=_yaml/talks.yml
+SITEMAP_YML=_yaml/sitemap.yml
+
+echo "links:" > $ESSAYS_YML
+
+echo "links:" > $SITEMAP_YML
+echo "  - https://cnr.sh/essays" >> $SITEMAP_YML
+echo "  - https://cnr.sh/talks" >> $SITEMAP_YML
 
 # Render essays
 for ESSAY_MD in $( ls -r _essays ); do
@@ -17,23 +20,26 @@ for ESSAY_MD in $( ls -r _essays ); do
   # That gnarly sed is to remove markdown links. Yeah.
   DESCRIPTION=$( sed -n '6 p' _essays/$ESSAY_MD | sed "s|\[\([^]]*\)\](\([^)]*\))|\1|g" | tr -d '"' | cut -c-200)
 
-  echo "  - title: $TITLE" >> $ESSAYS_YAML
-  echo "    date: $DATE" >> $ESSAYS_YAML
-  echo "    url: /essays/$ESSAY" >> $ESSAYS_YAML
+  echo "  - title: $TITLE" >> $ESSAYS_YML
+  echo "    date: $DATE" >> $ESSAYS_YML
+  echo "    url: /essays/$ESSAY" >> $ESSAYS_YML
 
-  echo "https://cnr.sh/essays/$ESSAY" >> $SITEMAP_TXT
+  echo "  - https://cnr.sh/essays/$ESSAY" >> $SITEMAP_YML
 
   pandoc --standalone -f markdown-implicit_figures --template _templates/essay.html -Mdescription="$DESCRIPTION" _essays/$ESSAY_MD -o essays/$ESSAY_HTML
 done
 
 # Render essays index page
-pandoc -f markdown-implicit_figures --template _templates/links.html -Mtitle=Essays --metadata-file=$ESSAYS_YAML -o essays/index.html < /dev/null
+pandoc -f markdown-implicit_figures --template _templates/links.html -Mtitle=Essays --metadata-file=$ESSAYS_YML -o essays/index.html < /dev/null
 
 # Render talks index page
-pandoc -f markdown-implicit_figures --template _templates/links.html -Mtitle=Talks --metadata-file=$TALKS_YAML -o talks/index.html < /dev/null
+pandoc -f markdown-implicit_figures --template _templates/links.html -Mtitle=Talks --metadata-file=$TALKS_YML -o talks/index.html < /dev/null
 
 # Render index page
 pandoc -f markdown-implicit_figures --template _templates/index.html index.md -o index.html
+
+# Render sitemap
+pandoc -f markdown-implicit_figures --template _templates/sitemap.xml --metadata-file=$SITEMAP_YML -t plain -o sitemap.xml < /dev/null
 
 # Render 404
 pandoc -f markdown-implicit_figures --template _templates/404.html 404.md -o 404.html
--- _scripts/sitemap.sh
+++ _scripts/sitemap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -s "http://www.google.com/ping?sitemap=https://cnr.sh/sitemap.txt" > /dev/null
+curl -s "http://www.google.com/ping?sitemap=https://cnr.sh/sitemap.xml" > /dev/null
