e6cc139bc9b67730c1ff1a677b99ed400b4179e2
Chris Riccomini <criccomini@apache.org>
2025-02-07 18:27:15 -0800

    Clean site and exclude 404

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ cp("assets/css/*.css", "site/css")
 cp("assets/js/*.js", "site/js")
 cp("assets/images/*.png", "site/images")
 cp("assets/*.ico", "site")
-cp("content/**/*.md", "site")
 cp("assets/*.txt", "site")
+cp("content/**/*.md", "site")
 
 # Update markdown frontmatter
 title("site/**/*.md")
@@ -25,6 +25,16 @@ feed(
     feed_description="Chris Riccomini's blog",
 )
 sitemap("site/**/*.md", site_url="https://cnr.sh")
+
+# Remove "404.md" from the root's sibling frontmatter
+def _strip_404(md, _):
+    frontmatter = md.frontmatter()
+    siblings = frontmatter.get("siblings", [])
+    siblings = filter(lambda s: s != "404.md", siblings)
+    md.update_frontmatter({"siblings": list(siblings)})
+    md.save()
+# TODO remove * when transform handles single file paths
+transform("site/index*.md", _strip_404)
 
 # Render site
 render("site/**/*.md", site={"title": "cnr.sh"})
